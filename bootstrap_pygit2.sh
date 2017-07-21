efd2170831bc0da86ac10c38ac22326cc09f99a0
root <root@ecp-elk-vm.cprcvj3safpe3pvaudmtx4lfsa.ax.internal.cloudapp.net>
2017-07-21 12:42:51 +0000

    updated pygit2 install script

--- bootstrap_pygit2.sh
+++ bootstrap_pygit2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-apt-get -y install cmake libssl-dev
+apt-get -y install cmake libssl-dev libffi-dev python-pip
 wget https://github.com/libgit2/libgit2/archive/v0.25.0.tar.gz
 tar xfz v0.25.0.tar.gz
 cd libgit2-0.25.0/
@@ -8,5 +8,5 @@ cmake .
 make
 sudo make install
 ldconfig
-pip install pygit2
+pip install pygit2==0.25.0
 
